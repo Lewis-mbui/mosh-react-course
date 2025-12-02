cd28d560bc348c139d7dd6e478899de2c6779b52
Lewis-mbui <lewmugo14479@gmail.com>
2025-12-02 18:15:23 +0300

    learn props

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
-import ListGroup from "./components/01-ManageState";
+import ListGroup from "./components/02-Props";
 
 function App() {
+  let items = ["New York", "San Fransisco", "Tokyo", "London", "Paris"];
+
   return (
     <div>
-      <ListGroup />
+      <ListGroup items={items} heading="Cities" />
     </div>
   );
 }
---
+++ src/components/02-Props.tsx
@@ -0,0 +1,39 @@
+import { useState } from "react";
+
+// { items: [], heading: string }
+interface Props {
+  items: string[];
+  heading: string;
+}
+
+// Destructuring
+function ListGroup({ items, heading }: Props) {
+  // Hook
+  const [selectedIndex, setSelectedIndex] = useState(-1);
+
+  return (
+    <>
+      <h1>{heading}</h1>
+      {items.length === 0 && <p>No item found</p>}
+      <ul className="list-group">
+        {items.map((item, index) => (
+          <li
+            className={
+              selectedIndex === index
+                ? "list-group-item active"
+                : "list-group-item"
+            }
+            onClick={() => {
+              setSelectedIndex(index);
+            }}
+            key={item}
+          >
+            {item}
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+}
+
+export default ListGroup;
