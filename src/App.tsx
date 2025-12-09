5de2b08bd92741f20ec1304a3ae2b52dd9e8e78f
Lewis-mbui <lewmugo14479@gmail.com>
2025-12-09 10:57:23 +0300

    learn how to pass children to a component

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,11 @@
-import ListGroup from "./components/03-PropsFunctions";
+import Alert from "./components/04-PassingChildren";
 
 function App() {
-  let items = ["New York", "San Fransisco", "Tokyo", "London", "Paris"];
-
-  const handleSelectItem = (item: string) => {
-    console.log(item);
-  };
-
   return (
     <div>
-      <ListGroup
-        items={items}
-        heading="Cities"
-        onSelectItem={handleSelectItem}
-      />
+      <Alert>
+        Hello <span>World</span>
+      </Alert>
     </div>
   );
 }
