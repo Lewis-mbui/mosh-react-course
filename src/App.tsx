cd54d75ca41236910e87ca73e2a3197a35acccaf
Lewis-mbui <lewmugo14479@gmail.com>
2025-12-23 18:25:20 +0300

    project: integrating RHF and Zod

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,9 @@
 // import Form from "./components/Form";
 import { useState } from "react";
+import categories from "./expense-tracker/categories";
 import ExpenseList from "./expense-tracker/components/ExpenseList";
 import ExpenseFilter from "./expense-tracker/components/ExpenseFilter";
 import ExpenseForm from "./expense-tracker/components/ExpenseForm";
-
-export const categories = ["Groceries", "Utilities", "Entertainment"];
 
 function App() {
   const [selectedCategory, setSelectedCategory] = useState("");
